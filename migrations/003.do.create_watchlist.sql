4186e43402fb91177820c423469326dd07e1488e
Tami Williams <tami@asktami.com>
2020-05-21 12:40:11 -0400

    allow nulls in watchlist

--- migrations/003.do.create_watchlist.sql
+++ migrations/003.do.create_watchlist.sql
@@ -3,12 +3,12 @@ CREATE TABLE watchlist (
     date_created TIMESTAMP DEFAULT now() NOT NULL,
     movie_id INTEGER NOT NULL,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
-   poster_path VARCHAR(255) NOT NULL DEFAULT '',
-    backdrop_path VARCHAR(255) NOT NULL DEFAULT '',
-    title VARCHAR(255) NOT NULL DEFAULT '',
-    original_title VARCHAR(255) NOT NULL DEFAULT '',
+   poster_path VARCHAR(255)  NULL DEFAULT '',
+    backdrop_path VARCHAR(255) NULL DEFAULT '',
+    title VARCHAR(255) NULL DEFAULT '',
+    original_title VARCHAR(255) NULL DEFAULT '',
     release_date DATE NULL,
-    overview VARCHAR NOT NULL DEFAULT '',
-    vote_average DECIMAL(3,0) NOT NULL DEFAULT 0.0,
-    vote_count INTEGER NOT NULL DEFAULT 0
+    overview VARCHAR NULL DEFAULT '',
+    vote_average DECIMAL(3,0) NULL DEFAULT 0.0,
+    vote_count INTEGER NULL DEFAULT 0
 );
