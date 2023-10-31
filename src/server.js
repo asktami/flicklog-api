fcaeec9172e367693ab9ae0138c0d28ad3d7adb8
Tami Williams <tami@asktami.com>
2023-10-31 18:52:41 -0400

    timeout updates

--- src/server.js
+++ src/server.js
@@ -5,6 +5,18 @@ const { PORT, DATABASE_URL } = require('./config');
 const db = knex({
 	client: 'pg',
 	connection: DATABASE_URL,
+	acquireConnectionTimeout: 1000000,
+	pool: {
+		min: 0,
+		max: 4,
+		acquireTimeoutMillis: 300000,
+		createTimeoutMillis: 300000,
+		destroyTimeoutMillis: 300000,
+		idleTimeoutMillis: 30000,
+		reapIntervalMillis: 1000,
+		createRetryIntervalMillis: 2000,
+	},
+	debug: false,
 });
 
 app.set('db', db);
